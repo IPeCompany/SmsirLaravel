bd2caaf5ad3032f046ecdc5f523aedea3d33ef47
IPeCompany <43531439+IPeCompany@users.noreply.github.com>
2018-12-10 11:05:16 +0330

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,5 @@
 <?php
-Route::group(['namespace'=>'ipecompany\smsirlaravel\Controllers','middleware'=>config('smsir.middlewares')], function (){
-	Route::get(config('smsir.route'),'SmsirController@index')->name('sms-admin');
-	Route::get('sms-admin/{log}/delete','SmsirController@delete')->name('deleteLog');
+Route::group(['namespace'=>'ipecompany\smsirlaravel\Controllers','middleware'=>config('smsirlaravel.middlewares')], function (){
+	Route::get(config('smsirlaravel.route'),'SmsirlaravelController@index')->name('sms-admin');
+	Route::get('sms-admin/{log}/delete','SmsirlaravelController@delete')->name('deleteLog');
 });
